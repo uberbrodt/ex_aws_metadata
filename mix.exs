f90bb1679f0e826a403867012a89c1d2025e7e03
Chris Brodt <chris@uberbrodt.net>
2017-10-10 08:12:27 -0500

    Added package info

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule AWSMetadata.Mixfile do
   def project do
     [
       app: :ex_aws_metadata,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      description: "Fetch, cache, and refresh AWS IAM Credentials",
+      package: package(),
       aliases: aliases(),
       deps: deps(),
     ]
@@ -23,6 +25,14 @@ defmodule AWSMetadata.Mixfile do
   defp aliases do
     [
       test: "test --no-start" 
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Chris Brodt chris@uberbrodt.net"],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/uberbrodt/ex_aws_metadata"}
     ]
   end
 
